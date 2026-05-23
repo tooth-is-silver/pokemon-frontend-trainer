19b878e55350378726b77b476ed86017392cd235
ga eun <hmyo2853@gmail.com>
2026-05-23 21:40:52 +0900

    feat: 심볼형 문제 세트 추가

--- src/content/questions/concept-pools.ts
+++ src/content/questions/concept-pools.ts
@@ -44,6 +44,7 @@ export const conceptPools: Record<string, string[]> = {
   ],
   "js-operators": ["+", "단항 +", "%", "**", "증가 연산자", "문자열 연결"],
   "js-switch": ["switch", "case", "default", "break", "fall-through", "==="],
+  "js-symbols": ["Symbol", "고유값", "심볼 키", "Symbol.for", "전역 레지스트리", "열거"],
   "js-types": ["number", "string", "boolean", "null", "undefined", "typeof"],
   "js-variables": ["let", "const", "var", "상수", "재할당", "하드코딩"],
 };
