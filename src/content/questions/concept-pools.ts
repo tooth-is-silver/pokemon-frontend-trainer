9128b7cf824b98b583569e786a1f60dc1baff7c5
ga eun <hmyo2853@gmail.com>
2026-05-24 22:26:06 +0900

    feat: 배열 문제 세트 추가

--- src/content/questions/concept-pools.ts
+++ src/content/questions/concept-pools.ts
@@ -1,5 +1,6 @@
 export const conceptPools: Record<string, string[]> = {
   "this-core": ["this", "렉시컬 환경", "클로저", "프로토타입", "스코프", "생성자 함수"],
+  "js-arrays": ["배열", "index", "push", "pop", "shift", "unshift"],
   "js-arrow-functions": [
     "=>",
     "화살표 함수",
