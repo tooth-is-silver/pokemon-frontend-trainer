5c267aad7ef94c4b0d4d3be52ecbe0e4a4063e01
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2026-05-26 23:54:27 +0900

    feat: 프로퍼티 접근자 문제 세트 추가 (#66)

--- src/content/questions/concept-pools.ts
+++ src/content/questions/concept-pools.ts
@@ -66,6 +66,7 @@ export const conceptPools: Record<string, string[]> = {
     "optional chaining",
   ],
   "js-operators": ["+", "단항 +", "%", "**", "증가 연산자", "문자열 연결"],
+  "js-property-accessors": ["getter", "setter", "get", "set", "접근자", "설명자"],
   "js-property-descriptors": [
     "descriptor",
     "writable",
--- src/content/questions/index.ts
+++ src/content/questions/index.ts
@@ -22,6 +22,7 @@ import { objectMethods } from "./by-page/object-methods";
 import { objectToPrimitive } from "./by-page/object-toprimitive";
 import { optionalChaining } from "./by-page/optional-chaining";
 import { operators } from "./by-page/operators";
+import { propertyAccessors } from "./by-page/property-accessors";
 import { propertyDescriptors } from "./by-page/property-descriptors";
 import { restParametersSpread } from "./by-page/rest-parameters-spread";
 import { switchStatement } from "./by-page/switch";
@@ -55,6 +56,7 @@ export const questionPages: QuestionPage[] = [
   weakMapWeakSet,
   nullishCoalescing,
   optionalChaining,
+  propertyAccessors,
   propertyDescriptors,
   restParametersSpread,
   whileFor,
