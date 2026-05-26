3554c57734704eb236f68dbcc879a49d1fcbc16b
ga eun <hmyo2853@gmail.com>
2026-05-26 23:33:02 +0900

    feat: 나머지 매개변수와 전개 문법 문제 세트 추가

--- src/content/questions/index.ts
+++ src/content/questions/index.ts
@@ -21,6 +21,7 @@ import { objectMethods } from "./by-page/object-methods";
 import { objectToPrimitive } from "./by-page/object-toprimitive";
 import { optionalChaining } from "./by-page/optional-chaining";
 import { operators } from "./by-page/operators";
+import { restParametersSpread } from "./by-page/rest-parameters-spread";
 import { switchStatement } from "./by-page/switch";
 import { symbolType } from "./by-page/symbol-type";
 import { jsTypes } from "./by-page/types";
@@ -51,6 +52,7 @@ export const questionPages: QuestionPage[] = [
   weakMapWeakSet,
   nullishCoalescing,
   optionalChaining,
+  restParametersSpread,
   whileFor,
   switchStatement,
   symbolType,
