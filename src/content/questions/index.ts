7cf06f8c5d858673a5faa608905cc05c3e7f7128
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2026-05-26 23:40:23 +0900

    feat: 나머지 매개변수와 전개 문법 문제 세트 추가 (#64)

--- src/content/questions/index.ts
+++ src/content/questions/index.ts
@@ -22,6 +22,7 @@ import { objectMethods } from "./by-page/object-methods";
 import { objectToPrimitive } from "./by-page/object-toprimitive";
 import { optionalChaining } from "./by-page/optional-chaining";
 import { operators } from "./by-page/operators";
+import { restParametersSpread } from "./by-page/rest-parameters-spread";
 import { switchStatement } from "./by-page/switch";
 import { symbolType } from "./by-page/symbol-type";
 import { jsTypes } from "./by-page/types";
@@ -53,6 +54,7 @@ export const questionPages: QuestionPage[] = [
   weakMapWeakSet,
   nullishCoalescing,
   optionalChaining,
+  restParametersSpread,
   whileFor,
   switchStatement,
   symbolType,
