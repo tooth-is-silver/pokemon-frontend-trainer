00a07cf59d98b323db7b0fb44dde733eaa67c339
ga eun <hmyo2853@gmail.com>
2026-05-29 17:46:48 +0900

    feat: async await 문제 세트 추가

--- src/content/questions/concept-pools.ts
+++ src/content/questions/concept-pools.ts
@@ -1,5 +1,6 @@
 export const conceptPools: Record<string, string[]> = {
   "this-core": ["this", "렉시컬 환경", "클로저", "프로토타입", "스코프", "생성자 함수"],
+  "js-async-await": ["async", "await", "Promise", "try...catch", "thenable", "Promise.all"],
   "js-arrays": ["배열", "index", "push", "pop", "shift", "unshift"],
   "js-array-methods": ["map", "filter", "find", "slice", "splice", "배열 메서드"],
   "js-arrow-functions": [
